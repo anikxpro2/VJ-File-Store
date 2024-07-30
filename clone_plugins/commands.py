ca38a748bc26253176ff6bde0e34aa9f0e048cdf
anikxpro2 <167355042+anikxpro2@users.noreply.github.com>
2024-07-30 13:28:50 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -100,7 +100,7 @@ async def start(client, message):
                 except:
                     return
             await msg.edit_caption(f_caption)
-        if AUTO_DELETE_MODE == True:
+if AUTO_DELETE_MODE == True:
     k = await msg.reply(
         f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</i></b>",
         quote=True
@@ -108,10 +108,10 @@ async def start(client, message):
     await asyncio.sleep(AUTO_DELETE_TIME)
     try:
         await x.delete()
-    except:
-        pass
+    except Exception as e:
+        # Log the exception if needed
+        print(f"Error deleting the file: {e}")
     await k.edit_text("<b>Your All Files/Videos is successfully deleted!!!</b>")
-
        
     files = files_[0]
     title = files.file_name
